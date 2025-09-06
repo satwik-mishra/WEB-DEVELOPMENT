1818492b0f29183eaf7a5b04d7bdcf63522b8077
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2025-09-06 12:15:46 +0530

    02_datatypes

---
+++ JAVASCRIPT_TUTORIALS/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+// alert("Hello") //we are using node js to run our code ,not browser so this will not work here
+let name="satwik" //string data type
+let age=20; //number data type
+let isLoggedIn=true; //boolean data type
+//data_types: 
+// 1. number
+// 2. string
+// 3. boolean
+// 4. bigInt
+// 5. null => it is a standalone value
+// 6. undefined => when a value is not assigned
+// 7. symbol => used for finding uniqueness
+console.log(typeof(name)); //string
+console.log(typeof(age)); //number
+console.log(typeof(isLoggedIn)); //boolean
+console.log(typeof null) //type of null is object
+console.log(typeof undefined); //undefined is a type itself
